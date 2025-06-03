2d22cdc17f0b42c0276c812b1fb82fed3ea2aecc
Sasorishi <on_alban@yahoo.fr>
2025-06-03 10:02:44 +0200

    add tailwind

--- frontend_safe/vite.config.js
+++ frontend_safe/vite.config.js
@@ -3,22 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     host: true,
-    allowedHosts: [
-      '7951-2a01-cb00-38f-9300-7593-6d0f-2caa-256c.ngrok-free.app'
-    ]
-  }
+    allowedHosts: ['7951-2a01-cb00-38f-9300-7593-6d0f-2caa-256c.ngrok-free.app'],
+  },
 })
